666e811a33b07b3c21106a8be3035488e580db61
Joshua Merlin <joshuay.merlin@gmail.com>
2018-08-29 15:10:45 -0400

    These files are my first Java programs.

---
+++ Geometry.java
@@ -0,0 +1,16 @@
+//Print the number of sides of several geometric shapes.
+public class Geometry {
+
+   public static void main (String[] args) {
+   int sides = 7; //declase and initialize
+   System.out.println ("A heptagon has " + sides + " sides.");
+   
+   sides = 10; //assignment statement
+   System.out.println ("A decagon has " + sides + " sides.");
+   
+   sides = 12; //assignment statement
+   System.out.println ("A dodecagon has " + sides + " sides.");
+   
+   }
+   
+}
