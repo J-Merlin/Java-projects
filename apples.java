ed26b0baf3725309947f14cc54ca215f1a8312f6
Joshua Merlin <joshuay.merlin@gmail.com>
2018-08-31 11:46:26 -0400

    Add files via upload
    These files I did via a YouTube tutorial from Thenewboston

---
+++ apples.java
@@ -0,0 +1,14 @@
+class apples{
+//once a class is pulled, the computer looks for a main method to use as a sort-of guide to read the instructions(code)
+
+//this line is known as the method header
+   public static void main(String args[]){
+      double tuna;
+      tuna = 5.28;
+      
+      System.out.print("I want ");
+      System.out.print(tuna);
+      System.out.print(" movies");
+      System.out.println(" apples");
+   }
+}
---
+++ scanner.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+class apples2{
+   public static void main(String args[]){
+      Scanner bucky = new Scanner(System.in);
+      System.out.println(bucky.nextLine());
+   }
+}
